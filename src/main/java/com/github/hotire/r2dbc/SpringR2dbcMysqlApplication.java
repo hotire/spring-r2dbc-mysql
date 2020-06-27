11584100dea63079e227f448d79dddefe74b4708
hotire <gngh0101@gmail.com>
2020-06-27 13:48:47 +0900

    init

---
+++ src/main/java/com/github/hotire/r2dbc/SpringR2dbcMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.hotire.r2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringR2dbcMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringR2dbcMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/hotire/r2dbc/SpringR2dbcMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hotire.r2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringR2dbcMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
